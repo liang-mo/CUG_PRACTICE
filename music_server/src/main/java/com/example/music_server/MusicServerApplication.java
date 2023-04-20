c9a7e80075932f7a882434d99914a08d2e63e572
lc2crj <137738655@qq.com>
2023-04-20 15:05:57 +0800

    基本css完善

--- music_server/src/main/java/com/example/music_server/MusicServerApplication.java
+++ music_server/src/main/java/com/example/music_server/MusicServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//1231
-@MapperScan("com.example.music_server.Dao")
+@MapperScan("com.example.music_server.Dao")//123
 public class MusicServerApplication {
 
 	public static void main(String[] args) {
