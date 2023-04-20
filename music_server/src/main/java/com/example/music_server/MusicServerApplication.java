d7a181bd65663d5ba6481e9791db16967a1b082a
cjx <2858114609@qq.com>
2023-04-20 09:31:25 +0800

    测试jenkins

--- music_server/src/main/java/com/example/music_server/MusicServerApplication.java
+++ music_server/src/main/java/com/example/music_server/MusicServerApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//123456
+@SpringBootApplication//12345678
 @MapperScan("com.example.music_server.Dao")
 public class MusicServerApplication {
 
