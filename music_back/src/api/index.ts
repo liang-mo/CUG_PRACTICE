9eef3e436d77653c8992f6eb573a3ee342051fa3
Qingmmmm <1099376859@qq.com>
2023-04-28 20:41:56 +0800

    --test=1018861 --user=周航 用户信息列表展示

--- music_back/src/api/index.ts
+++ music_back/src/api/index.ts
@@ -8,6 +8,22 @@ const HttpManager = {
   getLoginStatus: (params) => post(`admin/login/status`, params),
 
   // =======================> 用户 API
+  // 返回所有用户
+  getAllUser: () => get(`user`),
+  // 返回指定ID的用户
+  getUserOfId: (id) => get(`user/detail?id=${id}`),
+  // 添加用户
+  setUser: (params) => post(`user/add`, params),
+  // 更新用户信息
+  updateUserMsg: (params) => post(`user/update`, params),
+  // 删除用户
+  deleteUser: (id) => get(`user/delete?id=${id}`),
+
+  // =======================> 收藏列表 API
+  // 返回的指定用户ID收藏列表
+  getCollectionOfUser: (userId) => get(`collection/detail?userId=${userId}`),
+  // 删除收藏的歌曲
+  deleteCollection: (userId, songId) => deletes(`collection/delete?userId=${userId}&&songId=${songId}`),
 
 }
 
