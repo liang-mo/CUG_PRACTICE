b910ef246445ab88d5a8a4bdb96171e4d7fea625
cjx <2858114609@qq.com>
2023-05-06 11:59:02 +0800

    --story=1018392 --user=陈佳祥 歌手模块 https://www.tapd.cn/41153697/s/1057104

---
+++ music_server/src/main/java/com/example/music_server/service/SongListService.java
@@ -0,0 +1,22 @@
+package com.example.music_server.service;
+
+import com.example.music_server.domain.SongList;
+
+import java.util.List;
+
+public interface SongListService {
+
+    boolean addSongList (SongList songList);
+
+    boolean updateSongListMsg(SongList songList);
+
+    boolean updateSongListImg(SongList songList);
+
+    boolean deleteSongList(Integer id);
+
+    List<SongList> allSongList();
+
+    List<SongList> likeTitle(String title);
+
+    List<SongList> likeStyle(String style);
+}
