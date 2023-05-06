220126d528201e0c196a2fd4421d4750974350ed
cjx <2858114609@qq.com>
2023-05-06 12:31:33 +0800

    --task=1019864 --user=陈佳祥 歌曲接口 https://www.tapd.cn/41153697/s/1057108

---
+++ music_server/src/main/java/com/example/music_server/service/RankListService.java
@@ -0,0 +1,13 @@
+package com.example.music_server.service;
+
+import com.example.music_server.domain.RankList;
+
+public interface RankListService {
+
+    boolean addRank(RankList rankList);
+
+    int rankOfSongListId(Long songListId);
+
+    int getUserRank(Long consumerId, Long songListId);
+
+}
