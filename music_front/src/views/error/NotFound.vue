f9cbbcc51fa3c17ffd860d8c8588886b2e0e7d8e
jiaojiaozi511 <zzyswitch20020322@163.com>
2023-05-07 11:20:24 +0800

    前端主页及部分基础部件

---
+++ music_front/src/views/error/NotFound.vue
@@ -0,0 +1,26 @@
+<!--错误信息显示页面-->
+<template>
+  <div class="error-page">
+    <div class="error-code">Not Found</div>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  width: 100%;
+  height: 100%;
+  padding: 11rem 0;
+  box-sizing: border-box;
+}
+
+.error-code {
+  font-size: 250px;
+  font-weight: bolder;
+  color: #000000;
+}
+</style>
